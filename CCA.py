5851382fc1348047ed19489dfb7f89ed8263e846
Passant Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-05-20 13:28:31 +0200

    fix: :ambulance: fixxed helper functions for RSA keys generation
    update generate_e function to compute e and d, in addition to rewriting Miller_Rabin_Primality_Test function to follow the algorithm steps stated in wikipedia article, also updated generate_primes to use the new Miller_Rabin_Test function

--- CCA.py
+++ CCA.py
@@ -1,27 +1,25 @@
-from RSA import *
+from RSA2 import *
 from random import randint
-import math
-rsa = RSA(p=1000000007, q=1000000009, e=23917)
-
-message = "Take"
+rsa = RSA(bits_number=256)
+message = "You are watched !"
 
 cipher_text = rsa.encrypt(message=message)
 
-RANDOM_NUMBER = randint(1, int(math.log(cipher_text)))
+RANDOM_NUMBER = randint(1, 50)
 
 CHOSEN_CIPHER = rsa.encrypt(ConvertToStr(RANDOM_NUMBER))
 
 
-hacked_message =  rsa.decrypt(CHOSEN_CIPHER * cipher_text) // RANDOM_NUMBER
+hacked_message = rsa.decrypt(CHOSEN_CIPHER * cipher_text) // RANDOM_NUMBER
 
 
-# print(ConvertToStr(rsa.decrypt(cipher_text)))
-print("hacked message is: ", ConvertToStr(hacked_message))
-print("message is: ", message)
 
-if(hacked_message != message):
+if(ConvertToStr(hacked_message) != message):
     print('Attack Failed!')
 else:
     print('Attack Succeeded!')
     
-    
+# print(ConvertToStr(rsa.decrypt(cipher_text)))
+print("hacked message is: ", ConvertToStr(hacked_message))
+print("message is: ", message)
+
