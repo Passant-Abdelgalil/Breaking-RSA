8c7ecfb2840de081fb0f69266b75190195abd6c6
Passant Abdelgalil <basant.a.abdelgalil@gmail.com>
2022-05-21 16:56:37 +0200

    fix: :bug: Fixed Math Attack upon Reema's request <3

--- graphs.py
+++ graphs.py
@@ -39,20 +39,23 @@ if __name__ == "__main__":
         arr_message = ["a", "ab", "abc", 'abcd',
                     'ZZZZZ', 'reem12', 'reema12', 'reema123']
         arr_time = []
+        arr_n = []
         for bit, message in zip(arr_nBits, arr_message):
             rsa = RSA(bits_number=bit)
             cipher = rsa.encrypt(message=message)
+            arr_n.append(rsa.n)
 
             start = timeit.default_timer()
             msg_guessed_byAttacker = rsa.Math_Attack(
                 cipher=cipher, n=rsa.n, e=rsa.e)
             time_taken = timeit.default_timer() - start
+
             arr_time.append(time_taken)
             print(
-                f'time taken: {time_taken}, guessed message: {msg_guessed_byAttacker} with n bits: {bit}')
+                f'time taken: {time_taken}, guessed message: {msg_guessed_byAttacker} with bit length: {bit} and n: {rsa.n}')
 
         plt.title("nBits versus time taken by attack")
-        plt.plot(arr_nBits, arr_time, color="red")
+        plt.plot(arr_n, arr_time, color="red")
         plt.xlabel("Time")
         plt.xticks(arr_time)
         plt.ylabel("n bits")
